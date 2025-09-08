c55e64bf3dcd54370205885ce2696b6c98b7c214
Jee <zeddnyx08@gmail.com>
2025-09-08 07:38:39 +0700

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# TODO
+- option transform to lowercase
+- option change filename that have spaces to (-) or (_) 
+- option remove specific word
+- distribution across os
---
+++ src/main.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+
+fn renamed(i_name: []const u8) !void {
+    const allocator = std.heap.page_allocator;
+
+    var dir = try std.fs.cwd().openDir(".", .{ .iterate = true });
+    defer dir.close();
+    var dir_iterator = dir.iterate();
+
+    var file_list = std.ArrayList([]const u8).init(allocator);
+    defer file_list.deinit();
+
+    while (try dir_iterator.next()) |val| {
+        try file_list.append(val.name);
+    }
+
+    for (file_list.items) |name| {
+        const new_name = try std.fmt.allocPrint(allocator, "{s}-{s}", .{ i_name, name });
+        try std.fs.cwd().rename(name, new_name);
+        std.debug.print("old name: {s}, new name: {s}\n", .{ name, new_name });
+    }
+}
+
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+
+    const args = try std.process.argsAlloc(allocator);
+    defer allocator.free(args);
+
+    for (args[1..]) |arg| {
+        try renamed(arg);
+    }
+}
