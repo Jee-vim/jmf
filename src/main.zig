c93575a11ffcefdebf45da888783bc670e4a1199
Jee <zeddnyx08@gmail.com>
2025-09-14 08:53:34 +0700

    perf: improve how to using the args (using flag) refactor: change some variable name

--- src/main.zig
+++ src/main.zig
@@ -7,13 +7,25 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer allocator.free(args);
 
-    if (args.len < 2) {
-        std.debug.print("Usage: jren <value>\n", .{});
-        return;
+    var add_name: ?[]const u8 = null;
+    var rmv_name: ?[]const u8 = null;
+    var i: u8 = 1;
+
+    while (i < args.len) : (i += 1) {
+        if (std.mem.eql(u8, args[i], "-a")) {
+            i += 1;
+            if (i < args.len) add_name = args[i];
+        } else if (std.mem.eql(u8, args[i], "-r")) {
+            i += 1;
+            if (i < args.len) rmv_name = args[i];
+        }
     }
 
-    const add_name = args[1];
-    const rmv_name = if (args.len > 2) args[2] else null;
+    if (args.len < 2) {
+        std.debug.print("Usage: jren <command> <value>\n", .{});
+        std.debug.print("Ex: jren -a test-\n", .{});
+        return;
+    }
 
     lib.manipulationFile(.{ .add_name = add_name, .rmv_name = rmv_name }) catch |err| {
         std.debug.print("error: {s}\n", .{@errorName(err)});
--- src/utils/lib.zig
+++ src/utils/lib.zig
@@ -1,21 +1,21 @@
 const std = @import("std");
 
 const SRename = struct {
-    file_name: []const u8,
-    output: []const u8,
+    old_name: []const u8,
+    new_name: []const u8,
 };
 
-const Opt = struct {
-    add_name: []const u8,
+const SOpt = struct {
+    add_name: ?[]const u8,
     rmv_name: ?[]const u8,
 };
 
-pub fn rename(opt: SRename) !void {
-    try std.fs.cwd().rename(opt.file_name, opt.output);
-    std.debug.print("{s} -> {s}\n", .{ opt.file_name, opt.output });
+fn rename(opt: SRename) !void {
+    try std.fs.cwd().rename(opt.old_name, opt.new_name);
+    std.debug.print("{s} -> {s}\n", .{ opt.old_name, opt.new_name });
 }
 
-pub fn manipulationFile(opt: Opt) !void {
+pub fn manipulationFile(opt: SOpt) !void {
     const allocator = std.heap.page_allocator;
 
     var dir = try std.fs.cwd().openDir("./", .{ .iterate = true });
@@ -30,18 +30,19 @@ pub fn manipulationFile(opt: Opt) !void {
     }
 
     for (file_list.items) |file_name| {
-        const new_name = try std.fmt.allocPrint(allocator, "{s}{s}", .{ opt.add_name, file_name });
-        defer allocator.free(new_name);
+        if (opt.add_name) |val| {
+            const new_name = try std.fmt.allocPrint(allocator, "{s}{s}", .{ val, file_name });
+            defer allocator.free(new_name);
 
-        if (opt.rmv_name) |rmv| {
-            const size = std.mem.replacementSize(u8, file_name, rmv, "");
+            try rename(.{ .old_name = file_name, .new_name = new_name });
+        }
+        if (opt.rmv_name) |val| {
+            const size = std.mem.replacementSize(u8, file_name, val, "");
             const removed_name = try allocator.alloc(u8, size);
             defer allocator.free(removed_name);
-            _ = std.mem.replace(u8, file_name, rmv, "", removed_name);
+            _ = std.mem.replace(u8, file_name, val, "", removed_name);
 
-            try rename(.{ .file_name = file_name, .output = removed_name });
-        } else {
-            try rename(.{ .file_name = file_name, .output = new_name });
+            try rename(.{ .old_name = file_name, .new_name = removed_name });
         }
     }
 }
